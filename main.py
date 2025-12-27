d980b8e3a4ac9f1bd0185f0723fb360c6fdfc06f
Shubh Dewangan <shubhdwngn24@gmail.com>
2025-12-27 23:15:03 +0530

    refined using oops

--- main.py
+++ main.py
@@ -1,76 +1,119 @@
 from db import Database
 from security import hash_password, encrypt_password, decrypt_password
 
-def main():
-    db = Database()
-    print('---------> PASSWORD MANAGER\n')
+class PasswordManager:
+    def __init__(self):
+        self.db = Database()
+        self.user = None
 
-    username = input('Enter your username: ').strip()
-    #if user exists then true else false
-    user = db.get_user(username)
-
-    if not user:
-        master = input('Create a master password: ')
-        db.add_user(username, hash_password(master))
+    def signIn(self):
+        username = input("Enter your Username: ").strip()
+        check = self.db.get_user(username)
+        if check:
+            print('This user already exists!')
+            return False
+        
+        master_pass = input('Create your Master Password: ')
+        self.db.add_user(username, hash_password(master_pass))
         print('Account has been created!!!')
-        db.close()
-        return
+
+    def logIn(self):
+
+        username = input('Enter your Username: ').strip()
+        user = self.db.get_user(username)
+        if not user:
+            print(f'User with username {username} does not exists!')
+            return False
+        
+        master_pass = input('Enter your Master Password: ')
+        if hash_password(master_pass) != user[2]:
+            print('Wrong Password!')
+            return False
+        
+        self.user = user
+        print("You're successfully Logged In!")
+        return True
     
-    master = input('Enter your master password: ')
-    if hash_password(master) != user[2]:
-        print('Wrong password!')
-        db.close()
-        return
+    def addCredential(self):
+
+        site = input("Enter Site name: ").strip()
+        login_user = input("Login username (optional): ").strip()
+        pswd = input("Password: ").strip()
+        encrypted = encrypt_password(pswd)
+        self.db.add_vault(self.user[0], site, login_user, encrypted)
+        print("✅ Password saved!")
+
+    def viewCredential(self):
+
+        creds = self.db.get_vault(self.user[0])
+        if not creds:
+            print('No saved Credentials...!')
+            return
+        
+        for i in creds:
+            print(f"site : {i[0]}, username : {i[1]}, password : {decrypt_password(i[2])}")
     
-    print('You are Logged in!!')
+    def deleteCredential(self):
 
-    user_id = user[0]
+        site = input('Enter site name you want to delete: ')
+        self.db.delete_vault(self.user[0], site)
+        print(f'site : {site} has been deleted.')
+        return
+                
+    def menu(self):
+        while True:
+            print('\nPress 1 to Add Credential.')
+            print('Press 2 to see Credential(s).')
+            print('Press 3 to Delete Credential(s).')
+            print('Press {e} to Exit..')
 
-    while True:
-        print('\nPress 1 to Add Credentials.')
-        print('Press 2 to see your Credentials.')
-        print('Press 3 to delete your Credential(s).')
-        print('Press {e} to exit!\n')
+            choice = input('Enter your Choice: ').strip()
+            if choice == '1':
+                while True:
+                    self.addPassword()
+                    ask = input('Type {y} to confirm deleting more else {n}: ')
+                    if ask not in 'yY':
+                        break
+            elif choice == '2':
+                self.viewCredential()
 
-        choice = input('Enter your choice: ').strip()
+            elif choice == '3':
+                while True:
+                    self.deleteCredential()
+                    ask = input('Type {y} to confirm deleting more else {n}: ')
+                    if ask not in 'yY':
+                        break
+            
+            elif choice == 'e':
+                print('Thank You!!')
+                break
 
-        if choice == '1':
-            while True:
-                site = input('*Enter site name /app name /url: ')
-                login_user = input('Enter your login username (optional): ')
-                passwd = input('*Enter password: ')
-                db.add_vault(user_id, site, login_user, encrypt_password(passwd))
-                print('Credentials saved! ✅')
-                ask = input('Type {y} to add more otherwise {n}: ')
-                if ask not in 'yY':
-                    break
+            else:
+                print('Invalid Choice!!')
 
-        if choice == '2':
-            creds = db.get_vault(user_id)
-            print(creds) #debug remove later
-            if not creds:
-                print('No saved Credentials.')
-            for i in creds:
-                print(f"site : {i[0]}, username : {i[1]}, password : {decrypt_password(i[2])}")
-            break
+    def mainMenu(self):
+        print('----------------Password Manager----------------\n')
+        while True:
+            print('1. Sign Up your account!')
+            print('2. Log In to your Account!')
+            print('3. Exit...')
 
-        if choice == '3': # add a function where if site name not present then give another chance
-            while True:
-                site = input('Enter site name you want to delete: ')
-                db.delete_vault(user_id, site)
-                print(f'site : {site} has been deleted.')
-                choice = input('Type {y} to confirm deleting more else {n}: ')
-                if choice not in 'yY':
-                    break
+            choice = input('Enter your choice: ').strip()
+            if choice == '1':
+                self.signIn()
+            elif choice == '2':
+                if self.logIn():
+                    self.menu()
 
-        if choice.lower() == 'e':
-            break
+            elif choice == '3':
+                print('Thank you!')
+                break
 
-        else:
-            print('Invalid choice!!!')
+            else:
+                print('Invalid Choice!!')
 
-    db.close()
+        self.db.close()
 
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    appObj = PasswordManager()
+    appObj.mainMenu()
